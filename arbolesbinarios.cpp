cee64ee45633cb7a8dba26598ee13e7e688a8569
Neryed120819 <eduardo11.agr@gmail.com>
2019-11-13 19:54:33 -0600

    Version 2.0

--- arbolesbinarios.cpp
+++ arbolesbinarios.cpp
@@ -1,72 +1,75 @@
+//ARBOLES BINARIOS
 #include <iostream>
 #include <stdlib.h>
-#include <conio.h>
+#include<conio.h>
 using namespace std;
-struct Nodo{ 
-  int dato;
-  Nodo* izq,* der;
+
+struct Nodo{
+	int dato;
+	Nodo *der;
+	Nodo *izq;
 };
-Nodo* valorRaiz=NULL,*aux=NULL;
 
-int main() {
-    valorRaiz=new Nodo();
-    valorRaiz->dato=14;
-    valorRaiz->izq=new Nodo();
-    valorRaiz->der=new Nodo();
+void menu();
 
-    aux=valorRaiz->izq;
-    aux->dato=10;
-    aux->izq=new Nodo();
-    aux->der=new Nodo();
+Nodo *crearNodo(int);
+void agregarNodo(Nodo *&,int);
 
-    aux=aux->izq;
-    aux->dato=8;
-    aux->izq=NULL;
-    aux->der=NULL;
+Nodo *arbol = NULL;
 
-    aux=valorRaiz->izq;
-    aux=aux->der;
-    aux->dato=5;
-    aux->izq=NULL;
-    aux->der=NULL;
-
-    aux=valorRaiz->der;
-    aux->dato=3;
-    aux->izq=NULL;
-    aux->der=new Nodo();
-
-    aux=aux->der;
-    aux->dato=4;
-    aux->izq=new Nodo();
-    aux->der=NULL;
-
-    aux=aux->izq;
-    aux->dato=6;
-    aux->izq=NULL;
-    aux->der=NULL;
-    
-    cout<<"\t\t"<<valorRaiz->dato<<endl;
-    cout<<"\t"<<valorRaiz->izq->dato<<"\t\t "<<valorRaiz->der->dato<<endl;
-    cout<<"  "<<valorRaiz->izq->izq->dato<<"\t\t"<<valorRaiz->izq->der->dato<<"\t\t"<<valorRaiz->der->der->dato<<endl;
-    cout<<valorRaiz->der->der->izq->dato<<endl;
-
-
-    aux=valorRaiz->izq;
-    valorRaiz->izq=NULL;
-    delete aux;
-
-    aux=valorRaiz->der;
-    aux=aux->der;
-    aux=aux->izq;
-    delete aux;
-    aux=valorRaiz->der->der;
-    aux->izq=NULL;
-
-    cout<<endl<<"Al eliminar:"<<endl;
-    cout<<valorRaiz->dato<<endl;
-    cout<<valorRaiz->der->dato<<endl;
-    cout<<valorRaiz->der->der->dato<<endl;
-	system("pause");
-    return 0;
+int main(){
+	menu();
+	
+	getch();
+	return 0;
 }
 
+//Funcion del menu
+void menu(){
+	int dato, opcion;
+	
+	do{
+		cout<<"\t.:MENU:."<<endl;
+		cout<<"1.-Agregar un nuevo nodo"<<endl;
+		cout<<"2-Salir"<<endl;
+		cout<<"Elige una opcion: ";
+		cin>>opcion;
+		
+		switch(opcion){
+			case 1: cout<<"\nDame un numero: ";
+					cin>>dato;
+					agregarNodo(arbol,dato);
+					system("pause");
+					break;
+		}
+		system("cls");
+	}while(opcion != 2);
+}
+
+//Funcionp para crear un nuevo nodo
+Nodo *crearNodo(int n){
+	Nodo *nuevoNodo = new Nodo();
+	
+	nuevoNodo->dato = n;
+	nuevoNodo->der = NULL;
+	nuevoNodo->izq = NULL;
+	
+	return nuevoNodo;
+}
+//Funcion para agregar Nodo
+void agregarNodo(Nodo *&arbol, int n){
+	if (arbol == NULL){
+		Nodo *nuevoNodo = crearNodo(n);
+		arbol = nuevoNodo;
+	}
+	else{
+		int valorRaiz= arbol->dato;
+		if (n < valorRaiz){
+			agregarNodo(arbol->izq,n);
+		}
+		else{
+			agregarNodo(arbol->der,n);
+		}
+	}
+	
+}
